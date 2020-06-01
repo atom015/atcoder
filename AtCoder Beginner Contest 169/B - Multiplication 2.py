132952ac8f601f7467280c214499e90a382c9f16
김승우 <zeezlelove@gmail.com>
2020-06-01 23:16:06 +0900

    AtCoder Answer code

---
+++ AtCoder Beginner Contest 169/B - Multiplication 2.py
@@ -0,0 +1,13 @@
+N = int(input())
+arr = list(map(int,input().split()))
+
+if 0 in arr:
+    print(0)
+else:
+    r = 1
+    for i in arr:
+        if r*i > 10**18:
+            print(-1)
+            exit()
+        r *= i
+    print(r)
